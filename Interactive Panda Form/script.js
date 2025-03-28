a474e83d578ab3db89bc183901b968b593faf8f5
Luis Hanco <86266962+LuisHanco@users.noreply.github.com>
2025-03-28 13:27:37 -0500

    Add files via upload

---
+++ Interactive Panda Form/script.js
@@ -0,0 +1,93 @@
+let usernameRef = document.getElementById("username");
+let passwordRef = document.getElementById("password");
+let eyeL = document.querySelector(".eyeball-l");
+let eyeR = document.querySelector(".eyeball-r");
+let handL = document.querySelector(".hand-l");
+let handR = document.querySelector(".hand-r");
+
+let normalEyeStyle = () => {
+  eyeL.style.cssText = `
+    left:0.6em;
+    top: 0.6em;
+  `;
+  eyeR.style.cssText = `
+  right:0.6em;
+  top:0.6em;
+  `;
+};
+
+let normalHandStyle = () => {
+  handL.style.cssText = `
+        height: 2.81em;
+        top:8.4em;
+        left:7.5em;
+        transform: rotate(0deg);
+    `;
+  handR.style.cssText = `
+        height: 2.81em;
+        top: 8.4em;
+        right: 7.5em;
+        transform: rotate(0deg)
+    `;
+};
+//When clicked on username input
+usernameRef.addEventListener("focus", () => {
+  eyeL.style.cssText = `
+    left: 0.75em;
+    top: 1.12em;  
+  `;
+  eyeR.style.cssText = `
+    right: 0.75em;
+    top: 1.12em;
+  `;
+  normalHandStyle();
+});
+//When clicked on password input
+passwordRef.addEventListener("focus", () => {
+  handL.style.cssText = `
+        height: 6.56em;
+        top: 3.87em;
+        left: 11.75em;
+        transform: rotate(-155deg);    
+    `;
+  handR.style.cssText = `
+    height: 6.56em;
+    top: 3.87em;
+    right: 11.75em;
+    transform: rotate(155deg);
+  `;
+  normalEyeStyle();
+});
+// //When clicked outside username and password input
+ document.addEventListener("click", (e) => {
+ let clickedElem = e.target;
+if (clickedElem != usernameRef && clickedElem != passwordRef) {
+  normalEyeStyle();
+  normalHandStyle();
+ }
+});
+
+
+
+// seccion de boton
+
+let hints = [
+  "Es una palabra corta.",
+  "Tiene cinco letras.",
+  "Es un sustantivo relacionado con la infancia."
+];
+let hintIndex = 0;
+function checkPassword() {
+  let password = document.getElementById("password").value.toLowerCase();
+  let hintText = document.getElementById("hint");
+  if (password === "niña") {
+      window.location.href = "teamo.html";
+  } else {
+      if (hintIndex < hints.length) {
+          hintText.textContent = hints[hintIndex];
+          hintIndex++;
+      } else {
+          hintText.textContent = "Sigue intentando...";
+      }
+  }
+}
